3b99aa89b0ffd01439a4921d72e124769d239a4e
ArielAlael <arielalael@gmail.com>
2024-05-22 09:59:56 -0300

    modified button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,11 +4,11 @@ const Button = ({ text, src, icon, type = 'primary' }) => {
   const styleButton = useMemo(() => {
     switch (type) {
       case 'primary':
-        return 'flex items-center flex-row-reverse gap-2 bg-zinc-300 py-3 px-6 rounded-full hover:bg-zinc-400';
+        return 'cursor-pointer flex items-center flex-row-reverse gap-2 bg-zinc-300 py-3 px-6 rounded-full hover:bg-zinc-400';
       case 'secondary':
-        return 'flex items-center flex-row-reverse gap-2 bg-zinc-950 text-zinc-50 py-3 px-6 rounded-full hover:bg-zinc-800 ';
+        return 'cursor-pointer flex items-center flex-row-reverse gap-2 bg-zinc-950 text-zinc-50 py-3 px-6 rounded-full hover:bg-zinc-800 ';
       case 'tertiary':
-        return 'flex items-center flex-row-reverse gap-2 border border-zinc-300 bg-zinc-50 text-zinc-950 py-3 px-6 rounded-full hover:bg-zinc-950 hover:text-zinc-50 ';
+        return 'cursor-pointer flex items-center flex-row-reverse gap-2 border border-zinc-300 bg-zinc-50 text-zinc-950 py-3 px-6 rounded-full hover:bg-zinc-950 hover:text-zinc-50 ';
       default:
         break;
     }
